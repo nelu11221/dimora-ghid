af8ffcd5342d931cac04c19973d0587f4d5a8d73
nelu11221 <nelugarila70@gmail.com>
2026-06-02 01:32:13 +0300

    Rotate admin secret URL slug

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import Admin from './Admin';
 
 // Secret admin URL. Change this slug (and the backend ADMIN_PASSWORD) to keep it private.
-const ADMIN_PATH = '/admin-tramonto-7k9x';
+const ADMIN_PATH = '/admin-574c064b9972e9ed';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const isAdmin = window.location.pathname === ADMIN_PATH;
